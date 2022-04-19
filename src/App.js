36c9896dc375a71397d19ff37c5e335b3655e6aa
ramostravel <medramzi98@gmail.com>
2022-04-19 11:40:20 -0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React, { Component } from 'react'
+import imageSrc from './image.jpg'
+
+ class App extends Component {
+   constructor(props){
+     super(props);
+     this.state=
+     {
+    person:{
+      fullname:"ramzi",
+      bio:"acteur", 
+      profession:"dev",
+      imgSrc:imageSrc
+    },
+    
+     show:"false",
+     count: 0 ,
+    }
+    this.handleClick=this.handleClick.bind(this);
+  }
+
+  handleClick=()=>this.setState({show:!this.state.show});
+     
+  
+  componentDidMount() {
+      this.setState(
+        {
+          intervall: setInterval(
+            ()=>{this.setState({count: this.state.count + 1})
+            ;},1000
+        )
+    })}
+      handleShowPerson = () => {
+        this.setState({
+          ...this.state,
+          show: !this.state.show
+        });
+      };
+      
+      componentWillUnmount() {
+        
+      }
+    
+  render() {
+    return (
+      <div className='App-header'>
+        <div className={this.state.show ? "active" : ""}>
+<h1>hello my name is {this.state.fullname} , 
+<p>my bio is {this.state.bio}</p> ,
+my profession is {this.state.profession}<br/>
+</h1>
+<img src={this.state.person.imgSrc} alt="profile"/>
+</div>
+<button onClick={this.handleClick}>clic me</button>
+{this.state.count}
+</div>
+    )
+  }
 }
 
-export default App;
+export default App
